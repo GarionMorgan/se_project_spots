b0476a1bf58d81f0fd6e332025c9be237c631f56
github.com/GarionMorgan <garmorgan14@yahoo.com>
2025-08-22 13:19:35 -0500

    Added JavaScript functionality for the edit and new post buttons

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+//Edit Profile Modal
+const editProfileBtn = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+
+//New Post Modal
+const newPostBtn = document.querySelector(".profile__add-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+
+// Event Listeners for Edit Profile Modal
+editProfileBtn.addEventListener("click", () => {
+  editProfileModal.classList.add("modal_is-opened");
+});
+
+editProfileCloseBtn.addEventListener("click", () => {
+  editProfileModal.classList.remove("modal_is-opened");
+});
+
+// Event Listeners for New Post Modal
+newPostBtn.addEventListener("click", () => {
+  newPostModal.classList.add("modal_is-opened");
+});
+newPostCloseBtn.addEventListener("click", () => {
+  newPostModal.classList.remove("modal_is-opened");
+});
